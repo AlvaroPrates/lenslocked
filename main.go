8213fca2074ccaa5133b7b423d6017486010f811
Álvaro Prates <alvarobirckprates@gmail.com>
2023-11-25 15:03:59 -0300

    Section 6 - Starting to apply MVC

--- main.go
+++ main.go
@@ -2,49 +2,26 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/AlvaroPrates/lenslocked/controllers"
+	"github.com/AlvaroPrates/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
 
-func executeTemplate(w http.ResponseWriter, filePath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	tpl, err := template.ParseFiles(filePath)
-	if err != nil {
-		log.Printf("processing template: %v", err)
-		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
-		return
-	}
-
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-		return
-	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "home.html"))
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "contact.html"))
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "faq.html"))
-}
-
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	homeTpl := views.Must(views.Parse(filepath.Join("templates", "home.html")))
+	r.Get("/", controllers.StaticHandler(homeTpl))
+
+	contactTpl := views.Must(views.Parse(filepath.Join("templates", "contact.html")))
+	r.Get("/contact", controllers.StaticHandler(contactTpl))
+
+	faqTpl := views.Must(views.Parse(filepath.Join("templates", "faq.html")))
+	r.Get("/faq", controllers.StaticHandler(faqTpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
